d1aec81db3ef56a20fb2b07c391cac3f749664ff
james.chang <james.chang@xrspace.io>
2021-03-31 13:45:07 +0800

    feat: Learn how to get the spectrum data on Unity

---
+++ Assets/Scripts/SpectrumDataProvider.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SpectrumDataProvider : MonoBehaviour
+{
+    public const int Samples = 512;
+
+    public float[] SpectrumData => m_SpectrumData;
+
+    private void Update()
+    {
+        _GetSpectrumDataFromAudioSource();
+    }
+
+    private void _GetSpectrumDataFromAudioSource()
+    {
+        AudioSource?.GetSpectrumData(m_SpectrumData, 0, FFTWindow.BlackmanHarris);
+    }
+
+    private float[] m_SpectrumData = new float[Samples];
+
+    private AudioSource m_AudioSource;
+    private AudioSource AudioSource
+    {
+        get
+        {
+            if (m_AudioSource == null)
+                m_AudioSource = GetComponent<AudioSource>();
+
+            return m_AudioSource;
+        }
+    }
+}
---
+++ Assets/Scripts/SpectrumDataVisualator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SpectrumDataVisualator : MonoBehaviour
+{
+    [SerializeField]
+    private SpectrumDataProvider m_Provider;
+
+    [SerializeField]
+    private GameObject m_CubePrefab;
+
+    [SerializeField]
+    private float m_MaxScale;
+
+    private void Start()
+    {
+        if (m_CubePrefab == null) return;
+
+        for (int i = 0; i < SpectrumDataProvider.Samples; ++i)
+        {
+            var inst = Instantiate(m_CubePrefab, this.transform, false);
+            inst.name = "Sample_" + i;
+
+            Quaternion angle = Quaternion.Euler(0f, 360f / 512f * i, 0f);
+            inst.transform.position = this.transform.position + angle * Vector3.forward * 100;
+            inst.transform.rotation = angle;
+
+            m_SmpleCubes[i] = inst;
+
+            //this.transform.eulerAngles = new Vector3(0f, 360f/512f * i, 0f);
+            //inst.transform.position = this.transform.forward * 100;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_Provider == null) return;
+
+        for (int i = 0; i < SpectrumDataProvider.Samples; ++i)
+        {
+            if (m_SmpleCubes[i] == null) continue;
+
+            m_SmpleCubes[i].transform.localScale = new Vector3(1, (m_Provider.SpectrumData[i] * m_MaxScale) + 1, 1);
+        }
+    }
+
+    private GameObject[] m_SmpleCubes = new GameObject[SpectrumDataProvider.Samples];
+}
