aa7253520cb2cc141f82bbbc9d498ca7a4fa004e
james.chang <james.chang@xrspace.io>
2021-04-09 10:42:17 +0800

    feat: Learn how to use shader display specturm datas

---
+++ Assets/Scripts/AudioSpecturmMeter.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class AudioSpecturmMeter : MonoBehaviour
+{
+    [Range(10, 500)] 
+    public float value = 100.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        spectrumDatas = new float[windowSize];
+        spectrumDatasBuffer = new float[windowSize];
+
+        specturnBandTexture = new Texture2D(windowSize, 1, TextureFormat.R8, false)
+        {
+            filterMode = FilterMode.Point,
+            wrapMode = TextureWrapMode.Clamp
+        };
+
+        Shader.SetGlobalTexture("_SpecturmDataTex", specturnBandTexture);
+    }
+
+    private void OnDestroy()
+    {
+        if (specturnBandTexture != null)
+            Destroy(specturnBandTexture);
+        specturnBandTexture = null;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        AudioListener.GetSpectrumData(spectrumDatas, 0, FFTWindow.Rectangular);
+
+        for (int i = 0; i < windowSize; ++i)
+        {
+            float a = Mathf.Atan(i * 0.05f);
+
+            if (spectrumDatas[i] > spectrumDatasBuffer[i])
+            {
+                spectrumDatasBuffer[i] = spectrumDatas[i];
+            }
+            else
+            {
+                spectrumDatasBuffer[i] *= 0.98f;
+                if (spectrumDatasBuffer[i] < 0)
+                    spectrumDatasBuffer[i] = 0;
+            }
+
+            specturnData.r = spectrumDatasBuffer[i] * value * a;
+            specturnBandTexture.SetPixel(i, 0, specturnData);
+        }
+        specturnBandTexture.Apply();
+
+        //for (int x = 0; x < 4; ++x)
+        //{
+        //    float f = 0f;
+        //    for (int y = 0; y < 32; ++y)
+        //        f += spectrumDatasBuffer[x * 32 + y] * value;
+
+        //    specturnData.r = f / 32f;
+        //    specturnBandTexture.SetPixel(x, 0, specturnData);
+        //}
+        //specturnBandTexture.Apply();
+
+       
+    }
+
+    private int windowSize = 64;
+
+    private Color specturnData;
+    private Texture2D specturnBandTexture;
+    private float[] spectrumDatas;
+    private float[] spectrumDatasBuffer;
+}
---
+++ Assets/Shaders/AudioSpecturmMeter.shader
@@ -0,0 +1,70 @@
+﻿Shader "XRKTV/SpecturmMeter"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "white" {}
+
+    }
+        SubShader
+    {
+        Tags { "RenderType" = "Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            sampler2D _SpecturmDataTex;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+            // get specturm data
+            half specturmData = tex2D(_SpecturmDataTex, half2(i.uv.x, 0)).r;
+
+            // Way 1
+            //half level = round(i.uv.y / 0.125) * 0.125;
+            //col.rgb *= step(level, specturmData);
+
+            // Way 2
+            //col.rgb = saturate(col.rgb* specturmData)* step(i.uv.y, specturmData);
+            
+            // Way 3
+            col.rgb *= step(i.uv.y, specturmData); 
+            col.a = 1;
+
+            return col;
+         }
+         ENDCG
+     }
+    }
+}
